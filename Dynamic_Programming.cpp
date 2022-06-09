945a5e4c434c2fc90b9a576d61592975c18b2738
harsitsinha <harsits20@iitk.ac.in>
2022-06-09 09:43:23 +0530

    second commmit

---
+++ Dynamic_Programming.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+#include <vector>
+#include <limits>
+#include <unordered_set>
+#include <set>
+#include <stack>
+#include <queue>
+#include <unordered_set>
+#include <unordered_map>
+
+using namespace std;
+
+void fastio(){
+	#ifndef ONLINE_JUDGE
+
+	freopen("input.txt", "r", stdin);
+
+	freopen("output.txt", "w", stdout);
+
+	#endif // ONLINE_JUDGE
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+}
+
+int main(){
+	fastio();
+	cout<<"hello";
+
+
+
+
+
+	
+}
+
+
+
+
+
