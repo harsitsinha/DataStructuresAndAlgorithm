00b032d6fd9cdd760217d1652d648e03e9337e1c
harsitsinha <harsits20@iitk.ac.in>
2022-06-12 15:33:58 +0530

    LCA

--- Dynamic_Programming.cpp
+++ Dynamic_Programming.cpp
@@ -86,6 +86,53 @@ int fibTabular(int n){
 
 }
 
+int lCA(string str1,string str2, int m, int n){
+	if(m==0||n==0) return 0;
+	if(str1[m-1]==str2[n-1]) return 1+lCA(str1,str2,m-1,n-1);
+	else 
+		return max(lCA(str1,str2,m,n-1),lCA(str1,str2,m-1,n));
+
+}
+int lCA_memo(string str1,string str2, int m, int n){
+	if(m==0||n==0) t[m][n]=0;
+	if(str1[m-1]==str2[n-1]) {
+		t[m][n]= 1+lCA_memo(str1,str2,m-1,n-1);
+		return t[m][n];
+
+	}
+	else {
+		t[m][n]= max(lCA_memo(str1,str2,m,n-1),lCA_memo(str1,str2,m-1,n));
+		return t[m][n];
+	}
+
+}
+
+int lCA_tab(string str1,string str2, int m, int n){
+	int dp[m+1][n+1] ;
+	memset(dp, -1, sizeof(dp));
+	for(int i =0;i<m;i++){
+		dp[i][0]=0;
+	}
+	for(int i =0;i<n;i++){
+		dp[0][i]=0;
+	}
+	for(int i =1;i<=m;i++){
+		for(int j=1;j<=n;j++){
+			
+			if(str1[i-1]==str2[j-1]){
+				dp[i][j]=1+dp[i-1][j-1];
+
+			}
+			else
+				dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+
+			
+				
+		}
+	}
+	return dp[m][n];
+}
+
 int main(){
 	fastio();
 	////0-1 KNAPSACK
@@ -95,17 +142,44 @@ int main(){
 	// cout<<knapsack(wt,val,50,n);
 
 	////0-1 KNAPSACK USING MEMOISATION
-	vector<int> wt = {10,20,30};
-	vector<int> val = {60,100,120};
-	int n = wt.size();
-	memset(t, -1, sizeof(t));
-	cout<<knapsack_memo(wt,val,50,n);
+	// vector<int> wt = {10,20,30};
+	// vector<int> val = {60,100,120};
+	// int n = wt.size();
+	// memset(t, -1, sizeof(t));
+	// cout<<knapsack_memo(wt,val,50,n);
+
 	////fibonaci using memoisation
 	// memset(memo,-1,sizeof(memo));
 	// cout<<fib(100);
 
 	////fibonaci using tabulation
 	// cout<<fibTabular(10);
+
+	////Lowest common suqsequense using recursion
+	// string str1 = "AXYZ";
+	// string str2 = "BAZ";
+	// int m = str1.length();
+	// int n = str2.length();
+	// cout<<lCA(str1,str2,m,n);
+
+
+
+	////Lowest common suqsequense using memoization
+	// string str1 = "AXYZ";
+	// string str2 = "BAZ";
+	// int m = str1.length();
+	// int n = str2.length();
+	// memset(t, -1, sizeof(t));
+	// cout<<lCA_memo(str1,str2,m,n);
+
+
+	////Lowest common suqsequense using tabulation
+	string str1 = "AXYZ";
+	string str2 = "BAZ";
+	int m = str1.length();
+	int n = str2.length();
+	cout<<lCA_tab(str1,str2,m,n);
+
 
 
 
